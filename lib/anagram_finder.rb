70b0f5f5d0193409ba5119f154f45e21e8b66e04
mireie <michael.reiersgaard@gmail.com>
2021-07-16 08:44:08 -0700

    pass word checker test

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -9,10 +9,25 @@ class AnagramFinder
   def anagram_finder()
     word1_array = @word1.upcase.split("").sort()
     word2_array = @word2.upcase.split("").sort()
-    if word1_array === word2_array 
-      return 'These words are anagrams'
-    else 
-      return 'These words are not anagrams'
+    if word_checker?() === true
+      if word1_array === word2_array
+        return "These words are anagrams"
+      else
+        return "These words are not anagrams"
+      end
+    else
+      "You need to input actual words!"
     end
+  end
+
+  def word_checker?()
+    vowels = "aeiou"
+    result = false
+    vowels.each_char do |vowel|
+      if @word1.include?(vowel) && @word2.include?(vowel)
+        result = true
+      end
+    end
+    result
   end
 end
--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -11,5 +11,9 @@ describe('#AnagramFinder') do
     input = AnagramFinder.new("Ruby","Bury")
     expect((input.anagram_finder)).to(eq('These words are anagrams'))
   end
+  it("will compare two words and return whether or not they are words by checking for at least one vowel") do
+    input = AnagramFinder.new("Rsby","Btry")
+    expect((input.anagram_finder)).to(eq('You need to input actual words!'))
+  end
 
 end
