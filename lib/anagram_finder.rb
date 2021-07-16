947a803a3b315a086c9b80ff8314768b1ba742f1
mireie <michael.reiersgaard@gmail.com>
2021-07-16 08:16:26 -0700

    project setup

---
+++ lib/anagram_finder.rb
@@ -0,0 +1,8 @@
+require ('pry')
+
+class AnagramFinder
+  def initialize(word1,word2)
+    @word1 = word1
+    @word2 = word2
+  end
+end
---
+++ spec/anagram_finder_spec.rb
@@ -0,0 +1,10 @@
+require('rspec')
+require('anagram_finder')
+require('pry')
+
+describe('#AnagramFinder') do
+  it("will compare two words and return whether or not they are anigrams") do
+    expect(("")).to(eq(""))
+  end
+
+end
