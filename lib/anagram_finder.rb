4e5be8e4d1054ec8a37271927c2db055cd03c5b1
mireie <michael.reiersgaard@gmail.com>
2021-07-16 08:20:06 -0700

    pass first test

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,8 +1,18 @@
-require ('pry')
+require ("pry")
 
 class AnagramFinder
-  def initialize(word1,word2)
+  def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
+  end
+
+  def anagram_finder()
+    word1_array = @word1.split("").sort()
+    word2_array = @word2.split("").sort()
+    if word1_array === word2_array 
+      return true
+    else 
+      return false
+    end
   end
 end
--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -4,7 +4,8 @@ require('pry')
 
 describe('#AnagramFinder') do
   it("will compare two words and return whether or not they are anigrams") do
-    expect(("")).to(eq(""))
+    input = AnagramFinder.new("ruby","bury")
+    expect((input.anagram_finder)).to(eq(true))
   end
 
 end
